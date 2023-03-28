f51b15876b075bf2289bc3848bb8aff341789009
Mandi Montgomery <montgomerymm1@g.cofc.edu>
2023-03-28 07:53:34 -0400

    commit

--- scripts/analysis.R
+++ scripts/analysis.R
@@ -3,14 +3,18 @@ library("ggsurvfit")
 library("gtsummary")
 library("tidycmprsk")
 library("condsurv")
-library(survival)
+library("survival")
 
-dat <- read.csv('')
+dat <- read.csv('./data/clean_data/ProjectSurvivalData.csv')
 head(R_Class_Final_Project_Survival_Data)
+dat <- dat[ , 1:5]
+head(dat)
+dat$Treatment <- factor(dat$Treatment, levels = c('C', 'T'))
 
-options(contrasts=c("contr.sum", "contr.poly"))
+# options(contrasts=c("contr.sum", "contr.poly"))
 fit1 <- lm(Consumed ~ Treatment*Hours, data=dat) 
+summary(fit1)
 
-let_mod1=survfit(Surv(Hours)~Treatment+Consumed, data=R_Class_Final_Project_Survival_Data)
+let_mod1=survfit(Surv(Consumed)~Treatment+Hours, data=dat)
 plot(let_mod1, ylab='Survival Probability', xlab='Hours')
 
