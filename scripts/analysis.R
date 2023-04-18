56d529f9083507ad4f412cf29ea176965c897b95
Mandi Montgomery <montgomerymm1@g.cofc.edu>
2023-04-17 23:31:09 -0400

    Commit

--- scripts/analysis.R
+++ scripts/analysis.R
@@ -6,11 +6,21 @@ library(dplyr)
 library(ggpubr)
 library(plyr)
 library(datarium)
+library(lattice)
+library(survival)
 install.packages("AICcmodavg")
-
+install.packages("base")
+library(dplyr)
+library(survival)
+library(survminer)
+install.packages("survminer")
 #plyr load first
 
 dat <- read.csv('./data/clean_data/ProjectSurvivalData.csv')
+
+datnymph <- read.csv('./data/clean_data/Nymphsurvival.csv')
+
+datnymph$Salinity=as.factor(datnymph$Salinity)
 
 #Find outliers in data set
 dat %>%
@@ -33,15 +43,22 @@ summary(twoway)
 interaction <- aov(Consumed ~ Hours*Treatment, data=dat)
 summary(interaction)
 
-#graph
+#nymph survival graph
+nymph_key <- list(x = NULL, y =NULL, corner = c(0,0),
+                  text = list(c("Control", "12ppt", "16ppt","20ppt")),
+                  lines = list(col = c("blue", "red", "yellow", "green")))
 
-library(AICcmodavg)
-
-model.set <- list(twoway, interaction)
-model.names <- c("twoway", "interaction")
-
-aictab(model.set, modnames = model.names)
+xyplot(Survival ~ Days, group = Salinity, main = "Nymph Survival", data = datnymph, type= "a", col=c("blue", "red", "yellow", "green"), key = nymph_key)
 
 
-#graph
-xyplot(Otime, group = Id, data = dtL.data, type = "b")
+#tadpole survival graph
+tad_key <- list(x = NULL, y = NULL, corner = c(0, 1),
+                text = list(c("Salinity", "Control")),
+                lines = list(col = c("red", "blue")))
+
+xyplot(Consumed ~ Hours, group = Treatment, main = "Tadpoles Consumed", data = dat, type= "a", col=c("blue", "red"), key = tad_key)
+
+#activity graphs
+
+
+
