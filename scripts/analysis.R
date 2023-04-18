b3e56879ca1eac9287c3aad031dee405d2510f16
Mandi Montgomery <montgomerymm1@g.cofc.edu>
2023-04-18 01:00:16 -0400

    commit

--- scripts/analysis.R
+++ scripts/analysis.R
@@ -1,4 +1,4 @@
-
+#Load packages
 library(rstatix)
 library(reshape)
 library(tidyverse)
@@ -8,19 +8,13 @@ library(plyr)
 library(datarium)
 library(lattice)
 library(survival)
-install.packages("AICcmodavg")
-install.packages("base")
 library(dplyr)
 library(survival)
 library(survminer)
-install.packages("survminer")
-#plyr load first
 
+# Load tadpole survival/nymph consumption data
 dat <- read.csv('./data/clean_data/ProjectSurvivalData.csv')
 
-datnymph <- read.csv('./data/clean_data/Nymphsurvival.csv')
-
-datnymph$Salinity=as.factor(datnymph$Salinity)
 
 #Find outliers in data set
 dat %>%
@@ -28,22 +22,35 @@ dat %>%
   identify_outliers(Consumed)
 
 
-#mean
+#Find the mean
 dat %>%
   group_by(Treatment, Hours, Consumed) %>%
   summarize(mean_consumed = mean(Consumed))
 
-#Anova test
+#Run two way anova/repeated measures anova
 ?anova_test
 
 twoway <- aov(Consumed ~  Treatment + Hours + Treatment:Hours + Error(factor(Container)),
               data=dat)
 summary(twoway)
 
+#See if there is an interaction/correlation between the amount consumed and treatment+time
+
 interaction <- aov(Consumed ~ Hours*Treatment, data=dat)
 summary(interaction)
 
-#nymph survival graph
+#Create a graph to visual tadpole survival/how many tadpoles nymphs consumed
+tad_key <- list(x = NULL, y = NULL, corner = c(0, 1),
+                text = list(c("Salinity", "Control")),
+                lines = list(col = c("red", "blue")))
+
+xyplot(Consumed ~ Hours, group = Treatment, main = "Tadpoles Consumed", data = dat, type= "a", col=c("blue", "red"), key = tad_key)
+
+
+#Load data for nymph survival
+datnymph <- read.csv('./data/clean_data/Nymphsurvival.csv')
+
+#Create a graph to visualize nymph survival rates
 nymph_key <- list(x = NULL, y =NULL, corner = c(0,0),
                   text = list(c("Control", "12ppt", "16ppt","20ppt")),
                   lines = list(col = c("blue", "red", "yellow", "green")))
@@ -51,29 +58,26 @@ nymph_key <- list(x = NULL, y =NULL, corner = c(0,0),
 xyplot(Survival ~ Days, group = Salinity, main = "Nymph Survival", data = datnymph, type= "a", col=c("blue", "red", "yellow", "green"), key = nymph_key)
 
 
-#tadpole survival graph
-tad_key <- list(x = NULL, y = NULL, corner = c(0, 1),
-                text = list(c("Salinity", "Control")),
-                lines = list(col = c("red", "blue")))
 
-xyplot(Consumed ~ Hours, group = Treatment, main = "Tadpoles Consumed", data = dat, type= "a", col=c("blue", "red"), key = tad_key)
-
-#Nymph activity
+#Load nymph activity data
 
 nymphact<-read.csv('./data/clean_data/Nymphactivity.csv')
 
+#Run a t-test on nymph activity between the control and salinity to see if there is any significance between treatments
 n_act <- t.test(nymphact$Control_activity, nymphact$Salinity_activity , var.equal = TRUE)
 n_act
 
+#Create a box and whisker plot to visualize nymph activity data
 ggboxplot(nymphact, x = "Treatment", y = "Activity", color = "Treatment", ylab = "Nymph Activity (proportion of time)", xlab = "Treatment")
 
-#Tadpole activity
-
+#Load tadpole activity data
 tadact <- read.csv('./data/clean_data/Tadpoleactivity.csv')
 
+#Run a t-test on tadpole activity between the control and salinity to see if there is any significance between treatments
 t_act <- t.test(tadact$Control_activity, tadact$Salinity_activity, var.equal = TRUE)
 t_act
 
+#Create a box and whisker plot to visualize tadpole activity data
 ggboxplot(tadact, x = "Treatment", y = "Activity", color = "Treatment", ylab = "Tadpole Activity (proportion of time)", xlab = "Treatment")
           
 
